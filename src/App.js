2214865a7694c8954b8384649a9ddf1595cb9d2f
Swapnil Sharma <swapnil.sharma1998@outlook.com>
2021-02-25 21:40:25 +0530

    main

--- src/App.js
+++ src/App.js
@@ -4,20 +4,47 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="classNavbar">
+        <nav id="nav-bar">
+            <ul>
+              <li><a className="nav-link" href = "#video">HOME</a></li>
+              <li><a className="nav-link" href = "#contacts">ABOUT</a></li>
+              <li><a className="nav-link" href = "#abstract">WORK</a></li>
+              <li><a className="nav-link" href = "#abstract">CONTACT</a></li>
+              <li><a className="nav-link" href = "#video">Github</a></li>
+              <li><a className="nav-link" href = "#contacts">Twitter</a></li>
+              <li><a className="nav-link" href = "#abstract">Facebook</a></li>
+
+            </ul>
+        </nav>
+      </div>
+      <div className="classTitle">
+        <h1 id="title">Stand</h1>
+        <h2 id="title">out</h2>
+        <p id="small">Welcome to the page, I hope you are having a great day.</p>
+      </div>
+
+      <div id="description">
+         <p>Hi, my name is <span id="span">Swapnil</span>. I'm a <span>Front-End</span> Developer. I work as a <span>Performance Tester</span>. Below are the <span>projects</span> I made and the <span>services</span> I provide.</p>
+      </div>
+      <div id="description">
+         <p>........................</p>
+      </div>
+      <div id="description">
+         <p>........................</p>
+      </div>
+      <div id="description">
+         <p>........................</p>
+      </div>
+      <div id="description">
+         <p>........................</p>
+      </div>
+      <div id="description">
+         <p>........................</p>
+      </div>
+      <div id="description">
+         <p>........................</p>
+      </div>
     </div>
   );
 }
