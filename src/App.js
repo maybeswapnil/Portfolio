55840f648c7803019a8239b1864849ccb5291038
Swapnil Sharma <swapnil.sharma1998@outlook.com>
2021-03-24 22:13:43 +0530

    asd

--- src/App.js
+++ src/App.js
@@ -59,7 +59,7 @@ class App extends React.Component {
           </div>
           <div id="description">
             <p id="mainDes">Hi, my name is <span id="span">Swapnil.</span> I'm a <span>Front-End</span> Developer.</p>
-            <p id="mainDes" className="work">I'm a stock photographer, freelance webdesigner, I can design cars, build and pilot fpv drones. I work as a <span id="work">Performance Tester.</span> Below are the <span>projects</span> I made and the <span>services</span> I provide</p>
+            <p id="mainDes" className="work">I'm a stock photographer, freelance webdesigner, I can design cars, build and pilot fpv drones. I work as a <span id="work">Performance Tester.</span> Below are the <span>projects</span> I made and the <span>services</span> I provide.</p>
             <br />
             <p id="secDes"></p>
           </div>
@@ -67,14 +67,16 @@ class App extends React.Component {
           <Pexel />
           <br />
           <br />
-          <p id="mainDes" className="work" style={{fontSize:"50px", paddingRight:"800px"}}>Other <span>Projects</span></p>
+          <p id="mainDes" className="work dod" style={{fontSize:"50px", paddingRight:"800px"}}>Other <span>Projects</span></p>
           <br />
           <br />
           <Slideshow />
           <br />
           <br />
-          <p id="mainDes">Thanks you for going through the page.</p>
-            <p id="mainDes" className="work">It's a work in progress so check back often!</p>
+          <div id="mot">
+              <p id="mainDes">Thanks you for going through the page.</p>
+              <p id="mainDes" className="work">It's a work in progress so check back often!</p>
+          </div>
           <br />
           <div id="footer">
             <footer>© 2021 COPYRIGHT hellochemo | Find me on the links provided. | Peace out.</footer>
